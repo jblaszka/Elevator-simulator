625d5c2a91f67b0f612bf8a27837b954d4b99613
asia3211 <joanna.blaszka@o2.pl>
2022-10-18 20:20:58 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import model.ElevatorsSystem;
 
 public class Main {
     public static void main(String[] args) {
-        ElevatorsSystem elevatorsSystem = new ElevatorsSystem(1);
+        ElevatorsSystem elevatorsSystem = new ElevatorsSystem(16);
         ElevatorController elevatorController = new ElevatorController(elevatorsSystem);
     }
 }
